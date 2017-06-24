29b19e15eed35b9d639081a5bef9fda10a8cd00b
TatiusPeretyn <tperetyn@gmail.com>
2017-06-24 15:02:41 +0200

    Projekt 1
    Pierwsza wersja

---
+++ JashukProjekt/JashukProjekt/Personell.Designer.cs
@@ -0,0 +1,454 @@
+﻿namespace JashukProjekt
+{
+    partial class Personell
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.NumericUpDown();
+            this.n3 = new System.Windows.Forms.NumericUpDown();
+            this.n4 = new System.Windows.Forms.NumericUpDown();
+            this.n5 = new System.Windows.Forms.NumericUpDown();
+            this.n6 = new System.Windows.Forms.NumericUpDown();
+            this.n7 = new System.Windows.Forms.NumericUpDown();
+            this.n8 = new System.Windows.Forms.NumericUpDown();
+            this.n9 = new System.Windows.Forms.NumericUpDown();
+            this.n10 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.n1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.total)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(12, 42);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(105, 20);
+            this.text.TabIndex = 2;
+            // 
+            // n1
+            // 
+            this.n1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n1.Location = new System.Drawing.Point(123, 42);
+            this.n1.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(103, 20);
+            this.n1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Post";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wage";
+            // 
+            // total
+            // 
+            this.total.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.total.Location = new System.Drawing.Point(123, 358);
+            this.total.Maximum = new decimal(new int[] {
+            1492199424,
+            23306,
+            0,
+            0});
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(103, 20);
+            this.total.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 276);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(105, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 172);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(105, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 146);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(105, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 120);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(105, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 94);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(105, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 68);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(105, 20);
+            this.textBox9.TabIndex = 15;
+            // 
+            // n2
+            // 
+            this.n2.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n2.Location = new System.Drawing.Point(123, 68);
+            this.n2.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(103, 20);
+            this.n2.TabIndex = 16;
+            // 
+            // n3
+            // 
+            this.n3.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n3.Location = new System.Drawing.Point(123, 95);
+            this.n3.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(103, 20);
+            this.n3.TabIndex = 17;
+            // 
+            // n4
+            // 
+            this.n4.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n4.Location = new System.Drawing.Point(123, 121);
+            this.n4.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(103, 20);
+            this.n4.TabIndex = 18;
+            // 
+            // n5
+            // 
+            this.n5.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n5.Location = new System.Drawing.Point(123, 147);
+            this.n5.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(103, 20);
+            this.n5.TabIndex = 19;
+            // 
+            // n6
+            // 
+            this.n6.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n6.Location = new System.Drawing.Point(123, 172);
+            this.n6.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(103, 20);
+            this.n6.TabIndex = 20;
+            // 
+            // n7
+            // 
+            this.n7.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n7.Location = new System.Drawing.Point(123, 198);
+            this.n7.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(103, 20);
+            this.n7.TabIndex = 21;
+            // 
+            // n8
+            // 
+            this.n8.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n8.Location = new System.Drawing.Point(123, 224);
+            this.n8.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(103, 20);
+            this.n8.TabIndex = 22;
+            // 
+            // n9
+            // 
+            this.n9.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n9.Location = new System.Drawing.Point(123, 250);
+            this.n9.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(103, 20);
+            this.n9.TabIndex = 24;
+            // 
+            // n10
+            // 
+            this.n10.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.n10.Location = new System.Drawing.Point(123, 276);
+            this.n10.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.n10.Name = "n10";
+            this.n10.Size = new System.Drawing.Size(103, 20);
+            this.n10.TabIndex = 25;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(164, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 25);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Staff\'s wage:";
+            // 
+            // Personell
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(238, 419);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.n10);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.button1);
+            this.Name = "Personell";
+            this.Text = "Personnel";
+            ((System.ComponentModel.ISupportInitialize)(this.n1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.total)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.NumericUpDown n1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown total;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.NumericUpDown n2;
+        private System.Windows.Forms.NumericUpDown n3;
+        private System.Windows.Forms.NumericUpDown n4;
+        private System.Windows.Forms.NumericUpDown n5;
+        private System.Windows.Forms.NumericUpDown n6;
+        private System.Windows.Forms.NumericUpDown n7;
+        private System.Windows.Forms.NumericUpDown n8;
+        private System.Windows.Forms.NumericUpDown n9;
+        private System.Windows.Forms.NumericUpDown n10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ JashukProjekt/JashukProjekt/StartForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace JashukProjekt
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(315, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(110, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "INFO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(315, 128);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(331, 167);
+            this.MinimumSize = new System.Drawing.Size(331, 167);
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Need Name";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
